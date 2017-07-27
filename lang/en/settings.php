a7495371db9a37fe9c1f54aab420b5c5c7f82444
Matthieu Estrada <ttamalfor@gmail.com>
2017-07-27 12:29:41 +0200

    Adding lang for settings

---
+++ lang/en/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['jenkins.url']    = 'Add your Jenkins url, without a salsh ending. i.e.: http://my-jenkins.com';
+$lang['jenkins.user']   = 'Set a Jenkins user with admin right if possible to display any job.';
+$lang['jenkins.token']  = 'Set the corresponding Jenkins user token. You can find it in "http://JENKINS_URL/user/USER/configure", then click on "Show API Token".';
---
+++ lang/fr/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['jenkins.url']    = 'Entrez l\'url de votre serveur Jenkins, sans slash à la fin.'
+$lang['jenkins.user']   = 'Entrez le nom de l\'utilisateur Jenkins, avec les droits admin si possible, pour pouvoir afficher n\'importe quel job.'
+$lang['jenkins.token']  = 'Ajoutez le toekn correspondant à l\'utilisateur. Vous pouvez le trouver en allant sur "http://JENKINS_URL/user/USER/configure", cliquez ensuite sur "Show API Token".'
+
