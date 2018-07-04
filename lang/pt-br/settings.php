0b3b1136e3ea3d025abec96f7311a1ed052885d4
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-04 22:45:39 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['jenkins.url']           = 'Adicione sua URL do Jenkins, sem a barra no final. por exemplo: http://my-jenkins.com';
+$lang['jenkins.user']          = 'Digite o ID de um usuário Jenkins, com o direito de visualizar qualquer job.';
+$lang['jenkins.token']         = 'Defina o token de usuário correspondente do Jenkins. Você pode encontrá-lo em "http: // JENKINS_URL / user / USER / configure", depois clicar em "Show API Token".';
